14e949904e84df73a00882b949b6f68fb38a793c
realadnaan-maulid <adnaan9658@gmail.com>
2024-06-25 08:21:55 +0300

    urm

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -7,16 +7,24 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <h4>Quiz App</h4>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+       <div className='idk'>
+        <div className='box'>
+        <h1>Sign in to your account</h1>
+        <p>Username<input placeholder='Username' type="text" className='Username-input' /></p>
+        <p>Password
+        <input placeholder='Password' type="text" className='Password-input'  />
+        </p>
+        <IonButton className='mybutton'>Submit</IonButton>
+        <p>Don't Have a account?<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Create An Account</a></p>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Reset Your Password!</a>        
+        </div>
+       </div>
+       {/* here put on the divs and all that */}
       </IonContent>
     </IonPage>
   );
